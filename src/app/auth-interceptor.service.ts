2333ca6999826ee5cfa6f41dea8b1edb7016c9be
beataj <beata@emc2wave.com>
2021-09-11 14:45:58 -0700

    add LoggingInterceptorService to app.module

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -1,5 +1,5 @@
 import { HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http'
-import { tap } from 'rxjs/operators';
+
 
 export class AuthInterceptorService implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler) {
