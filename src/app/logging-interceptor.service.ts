6c274e8d7fa02bd4d810702066fc694c7606f3cf
beataj <beata@emc2wave.com>
2021-09-11 14:43:18 -0700

    move logging part from one interceptor to otther

--- src/app/logging-interceptor.service.ts
+++ src/app/logging-interceptor.service.ts
@@ -1,7 +1,15 @@
-import { HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { HttpEventType, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { tap } from "rxjs/operators";
 
 export class LoggingInterceptorService implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler) {
-        return next.handle(req);
+        console.log('Outgoing request');
+        console.log(req.url);
+        return next.handle(req).pipe(tap(event => {
+            if(event.type === HttpEventType.Response) {
+                console.log('Incoming response');
+                console.log(event.body);
+            }
+        }));
     }
 }
